09736aca6abffa99061941203e0990994fa74d43
Nikita soni <nikitasoni.mindiii@gmail.com>
2023-03-22 19:13:05 +0530

    working on signup form common text folder

---
+++ src/CommonStaticText.js
@@ -0,0 +1,10 @@
+export const statictext ={
+    // Login/signup form text
+    CREATEACCOUNT : "CREATE ACCOUNT",
+    LOGIN: "LOGIN",
+    EMAIL_LOGIN: "EMAIL LOGIN",
+    REMEMBER_ACCOUNT:"Remember me",
+    FORGOT_PASSWORD: "Forgot Password?",
+
+   
+}
