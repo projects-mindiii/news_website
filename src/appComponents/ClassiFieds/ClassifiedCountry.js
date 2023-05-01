a0f2cb4cbf5519b629008a4a0d8db695bba839ce
Nikita soni <nikitasoni.mindiii@gmail.com>
2023-05-01 13:52:39 +0530

    working on classied filterdesign

--- src/appComponents/ClassiFieds/ClassifiedCountry.js
+++ src/appComponents/ClassiFieds/ClassifiedCountry.js
@@ -1,86 +1,116 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import mapicon from "../../assets/images/map_ico.png";
-import { useSelector } from "react-redux";
-import SublyApi from "../../helpers/Api";
-import "../ClassiFieds/ClassiFieds.css";
-import "./ClassifiedCountry.css"
+import Select from "react-select";
 
 
 function ClassifiedCountry() {
-  const [CountryName, setCountryName] = useState("");
-  const [CatergoryType, setCategoryType] = useState("");
-  const [selectCountry, setSelectCountry] = useState("");
-  const [countriesSelect, setcountriesSelect] = useState(false);
-  const [CatergoryTypeIndex, setCategoryTypeIndex] = useState(0);
-  const { userToken, currentUser, isLoading } = useSelector(
-    (state) => state.user
-  );
+  const [isOpen, setIsOpen] = useState(false);
+  const [locationSelected, setLocationSelected] = useState({
+    value: 1,
+    label: "All South Africa",
+    id: 1,
+  });
 
-  //function for get classiFieds metalist
-  // useEffect(() => {
-  //   async function getClassifiedLists() {
-  //     let requestData = new FormData();
-  //     requestData.append("limit", "");
-  //     requestData.append("offset", "");
-  //     requestData.append("type", 6);
-  //     requestData.append("search_by", 1);
-  //     requestData.append("province", 931);
-  //     requestData.append("country", 15);
-  //     requestData.append("city", "Free State");
-  //     await SublyApi.getClassiFiedMeta(userToken).then((responsejson) => {
-  //       if (responsejson.status_code == 500) {
-  //       } else if (responsejson.status_code == 400) {
-  //       } else {
-  //         if (responsejson.status_code == 200) {
-  //           setCategoryType(responsejson.data.category_type);
-  //           setCountryName(responsejson.data.provinces);
-  //         }
-  //       }
-  //     });
-  //     // SublyApi.getClassifiedList(requestData, userToken).then((responsejson) => {
-  //     // });
-  //   }
-  //   getClassifiedLists();
-  // }, []);
-
-
+  const locationOption = [
+    { value: 1, label: "All South Africa", id: 1 },
+    { value: 0, label: "Outside South Africa", id: 0 },
+    { value: 1, label: "All South Africa", id: 1 },
+    { value: 0, label: "Outside South Africa", id: 0 },
+    { value: 1, label: "All South Africa", id: 1 },
+    { value: 0, label: "Outside South Africa", id: 0 },
+    { value: 1, label: "All South Africa", id: 1 },
+    { value: 0, label: "Outside South Africa", id: 0 },
+  ];
+  const toggleSelectBox = () => {
+    setIsOpen(!isOpen);
+  }
  
   return (
+    <>
+
+
     <div className="classiFieds_map_serchbar">
       <div className="countryIcon">
-        {/* <span
-          onClick={() =>
-            setcountriesSelect(countriesSelect == true ? false : true)
-          }
-        >
-          <img src={mapicon} alt={mapicon} width="25px" height="25px" />
-          {"All South Africa "}
-
-          <span>
-            {selectCountry
-              ? selectCountry
-              : CountryName && CountryName.length > 0 && CountryName[0].name}
-          </span>
-        </span> */}
-        <span className="imgIcon"> <img src={mapicon} alt={mapicon} width="25px" height="25px" /><span className="countryText">All South Africa -<span className="resultText">0 Results</span> </span></span>
-        
-        <div className="classiFieds_country_select">
-          {CountryName &&
-            CountryName.length > 0 &&
-            CountryName.map((item, index) => (
-              <div
-                onClick={() => setSelectCountry(item.name)}
-                style={{
-                  display: countriesSelect == true ? "block" : "none",
-                }}
-              >
-                {item.name}
-              </div>
-            ))}
+        <div className="selectOption">
+        <img src={mapicon} alt={mapicon} width="25px" height="25px" onClick={toggleSelectBox}/>
+     <span className="countryText" onClick={toggleSelectBox}>
+          All South Africa -<span className="resultText">0 Results</span>{" "}
+        </span>
         </div>
       </div>
-     
+
     </div>
+   
+   {isOpen && (
+    <Select
+    id="location"
+    name="location"
+    options={locationOption}
+    onChange={setLocationSelected}
+    value={locationSelected}
+    styles={{
+      placeholder: () => ({
+        fontSize: "15px",
+        color: "#cacaca",
+        position: "absolute",
+        top: "8px",
+        left: "15px",
+      }),
+    }}
+    theme={(theme) => ({
+      ...theme,
+      colors: {
+        ...theme.colors,
+        borderRadius: 0,
+        primary25: "#f2f2f2",
+        primary: "#000000;",
+        primary50: "#f2f2f2",
+      },
+    })}
+  />
+  )}
+
+  </>
+
+    // <div>
+    //   <button onClick={() => setModalIsOpen(true)}>
+    //     <img src={mapicon} alt={mapicon} width="25px" height="25px" />
+    //     <span className="countryText">
+    //       All South Africa -<span className="resultText">0 Results</span>{" "}
+    //     </span>
+    //   </button>
+    //   <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+    //     <Select
+    //       id="location"
+    //       name="location"
+    //       options={locationOption}
+    //       onChange={setLocationSelected}
+    //       value={locationSelected}
+    //       styles={{
+    //         placeholder: () => ({
+    //           fontSize: "15px",
+    //           color: "#cacaca",
+    //           position: "absolute",
+    //           top: "8px",
+    //           left: "15px",
+    //         }),
+    //       }}
+    //       theme={(theme) => ({
+    //         ...theme,
+    //         colors: {
+    //           ...theme.colors,
+    //           borderRadius: 0,
+    //           primary25: "#f2f2f2",
+    //           primary: "#000000;",
+    //           primary50: "#f2f2f2",
+    //         },
+    //       })}
+    //     />
+    //     <button onClick={() => setModalIsOpen(false)}>Close Modal</button>
+    //   </Modal>
+    // </div>
   );
 }
+
 export default ClassifiedCountry;
