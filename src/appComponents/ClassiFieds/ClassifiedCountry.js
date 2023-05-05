43b8e3204de1f3b064f8813e32fa242a08a34102
Nikita soni <nikitasoni.mindiii@gmail.com>
2023-05-05 20:16:26 +0530

    working on classified design changes according to figma

--- src/appComponents/ClassiFieds/ClassifiedCountry.js
+++ src/appComponents/ClassiFieds/ClassifiedCountry.js
@@ -1,34 +1,54 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import mapicon from "../../assets/images/map_ico.png";
 import "../ClassiFieds/ClassiFieds.css";
 import { useTranslation } from "react-i18next";
-import styles from './ClassifiedCountry.module.css';
+import styles from "./ClassifiedCountry.module.css";
 import ClassifiedFilter from "./ClassifiedFilter";
-
+import { useDispatch, useSelector } from "react-redux";
+import { setClassfiedType } from "../../store/slices/ClassifiedSlice";
+import { CLASSIFIED_CATEGORY_TYPE } from "../../utils/Constants";
 
 function ClassifiedCountry() {
-  function closeModal(){
+  function closeModal() {
     return setIsOpen(false);
   }
+  const { forSaleTotalCount, wantedTotalCount } = useSelector(
+    (state) => state.classified
+  );
 
   const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
+  const [countryData, setCountryData] = useState("");
+  console.log("countryData", countryData);
 
   return (
     <div className={styles.classiFieds_map_serchbar}>
       <div className={styles.countryIcon} onClick={() => setIsOpen(true)}>
-        <span className={styles.imgIcon}>
-          {" "}
-          <img src={mapicon} alt={mapicon} width="25px" height="25px" />
-          
-          <span className={styles.countryText}>
-            All South Africa -<span className={styles.resultText}>0 Results</span>{" "}
-          </span>
-        </span>
+        <div className={styles.imgIcon}>
+          <img src={mapicon} alt={mapicon} width="25px" height="25px" />{" "}
+        </div>
+        <div className={styles.countryText}>
+          {countryData.label ? (
+            <div className={styles.countryText}>
+              {countryData.label} -
+              <span className={styles.resultText}>
+                {forSaleTotalCount} Results
+              </span>{" "}
+            </div>
+          ) : (
+            <p>
+              All South Africa - <span> 0 Result</span>
+            </p>
+          )}
+        </div>
       </div>
-      {isOpen && <ClassifiedFilter closeModal={closeModal}/>}
+      {isOpen && (
+        <ClassifiedFilter
+          closeModal={closeModal}
+          setCountryData={setCountryData}
+        />
+      )}
     </div>
   );
-  
 }
 export default ClassifiedCountry;
