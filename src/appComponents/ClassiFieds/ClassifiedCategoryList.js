6729b4b8c79f8f2547a2f0539bceb4b22b5aa1d6
Kishore Mahajan <kishore.mindiii@gmail.com>
2023-05-11 20:45:40 +0530

    update the advert

--- src/appComponents/ClassiFieds/ClassifiedCategoryList.js
+++ src/appComponents/ClassiFieds/ClassifiedCategoryList.js
@@ -10,10 +10,8 @@ import watchicon from "../../assets/images/watch_ico.png";
 import { useTranslation } from "react-i18next";
 import { CLASSIFIED_CATEGORY_TYPE } from "../../utils/Constants";
 import AddBookMarks from "../BookMarks/AddBookMarks";
-import { useNavigate } from "react-router-dom";
 
-function ClassifiedCategoryList({ forSaleListData, bookType,displayRoute }) {
-  const navigate =useNavigate();
+function ClassifiedCategoryList({ forSaleListData, bookType }) {
   const { t } = useTranslation();
   return (
     <div className="main">
@@ -21,9 +19,6 @@ function ClassifiedCategoryList({ forSaleListData, bookType,displayRoute }) {
         forSaleListData.length > 0 &&
         forSaleListData.map((item, index) => {
           return (
-            
-              displayRoute &&  displayRoute=="your_advert"  ?
-                item.approval_status == 1 &&
             <div className="classiFieds_forSaleBox">
               <div className="classiFieds_forSale_about">
                 <div className="classiFieds_forSale">
@@ -76,7 +71,7 @@ function ClassifiedCategoryList({ forSaleListData, bookType,displayRoute }) {
               {item.category_type_id == CLASSIFIED_CATEGORY_TYPE.FORSALE ||
               item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBOFFER ? (
                 <div className="classiFieds_RupeesText">
-                  {item.amount && item.amount !== 0 && (
+                  {item.amount !== 0 && (
                     <p>
                       {/* {item.currency_code} {item.amount ? item.amount : "0"}
                        {item.amount.toString().includes(".") ? "" : ".00"} */}
@@ -118,204 +113,6 @@ function ClassifiedCategoryList({ forSaleListData, bookType,displayRoute }) {
               </div>
 
               <SocialMedaiShare />
-              <button className="edit_DeleteButton" onClick={()=>navigate("/post-advert",{state:item})}>EDIT / DELETE ADVERT</button>
-            </div>:<div className="classiFieds_forSaleBox">
-              <div className="classiFieds_forSale_about">
-                <div className="classiFieds_forSale">
-                  <img src={item.img_url} alt={item.img_url} />
-                </div>
-                <div className="classiFields_heading">
-                  <p className="text">{item.heading}</p>
-                  <div className="classiFieds_time_action">
-                    <div className="classiFieds_time">
-                      <button
-                        className="classiFieds_forSale_button"
-                        style={{
-                          background: `linear-gradient(to right, ${item.color_code.start_color}, ${item.color_code.end_color})`,
-                          color: `text-color(to text color, ${item.color_code.text_color})`,
-                        }}
-                      >
-                        {item.category_name}
-                      </button>
-
-                      <span>
-                        <span>
-                          <img src={watchicon} alt={watchicon} />{" "}
-                        </span>{" "}
-                        {item.created_date}
-                      </span>
-                    </div>
-                    <div className="classiFieds_bookmarkicon">
-                    <AddBookMarks
-                        isBookmark={item.is_bookmark}
-                        id={item.id}
-                        bookType={bookType}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              {item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBOFFER ||
-              item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBSEEKERS ? (
-                <div className="jobType">
-                  <p>
-                    {item.job_type_name ? item.job_type_name + `-` : ""}
-                    {item.job_location_type_name}
-                  </p>
-                </div>
-              ) : (
-                ""
-              )}
-
-              {item.category_type_id == CLASSIFIED_CATEGORY_TYPE.FORSALE ||
-              item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBOFFER ? (
-                <div className="classiFieds_RupeesText">
-                  {item.amount && item.amount !== 0 && (
-                    <p>
-                      {/* {item.currency_code} {item.amount ? item.amount : "0"}
-                       {item.amount.toString().includes(".") ? "" : ".00"} */}
-                      {item.currency_code} {item.amount.toFixed(2)}
-                      {/* {item.currency_code} {new Intl.NumberFormat().format(item.amount)} */}
-                    </p>
-                  )}
-
-                  <span>
-                    {item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBOFFER
-                      ? item.earning_name
-                      : item.currency_name}
-                    {item.is_negotiable ? "(Negotiable)" : ""}
-                  </span>
-                </div>
-              ) : (
-                ""
-              )}
-
-              <div className="classiFieds_countryName ">
-                <span>
-                  <img src={mapicon} alt={mapicon} />
-                  <span>
-                    {(item.country_name)?item.country_name:""}{(item.province_name)?', '+item.province_name:""}{(item.city)?', '+item.city:""}
-                  </span>
-                </span>
-              </div>
-              <div className="classiFieds_aboutText">
-                <p>{item.description}</p>
-              </div>
-              <div className="classiFields_contactPerson">
-                <ContactPerson
-                  forSaleListData={forSaleListData}
-                  index={index}
-                />
-                {item.whatapp_contact_number.length > 0 && (
-                  <WhatsApp watsApp={true} />
-                )}
-              </div>
-
-              <SocialMedaiShare />
-            </div>
-          );
-        })}
-        {displayRoute &&  displayRoute=="your_advert" && forSaleListData && forSaleListData.some(item => item.approval_status == 0) == true && <h5 className="youAdd_PendingApproval">---- PENDING APPROVAL ----</h5>}
-         {forSaleListData &&
-        forSaleListData.length > 0 &&
-        forSaleListData.map((item, index) => {
-          return (
-            
-              displayRoute &&  displayRoute=="your_advert"  &&
-                item.approval_status == 0 &&
-            <div className="classiFieds_forSaleBox">
-              <div className="classiFieds_forSale_about">
-                <div className="classiFieds_forSale">
-                  <img src={item.img_url} alt={item.img_url} />
-                </div>
-                <div className="classiFields_heading">
-                  <p className="text">{item.heading}</p>
-                  <div className="classiFieds_time_action">
-                    <div className="classiFieds_time">
-                      <button
-                        className="classiFieds_forSale_button"
-                        style={{
-                          background: `linear-gradient(to right, ${item.color_code.start_color}, ${item.color_code.end_color})`,
-                          color: `text-color(to text color, ${item.color_code.text_color})`,
-                        }}
-                      >
-                        {item.category_name}
-                      </button>
-
-                      <span>
-                        <span>
-                          <img src={watchicon} alt={watchicon} />{" "}
-                        </span>{" "}
-                        {item.created_date}
-                      </span>
-                    </div>
-                    <div className="classiFieds_bookmarkicon">
-                      <img src={bookmarkicon} alt={bookmarkicon} />
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              {item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBOFFER ||
-              item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBSEEKERS ? (
-                <div className="jobType">
-                  <p>
-                    {item.job_type_name ? item.job_type_name + `-` : ""}
-                    {item.job_location_type_name}
-                  </p>
-                </div>
-              ) : (
-                ""
-              )}
-
-              {item.category_type_id == CLASSIFIED_CATEGORY_TYPE.FORSALE ||
-              item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBOFFER ? (
-                <div className="classiFieds_RupeesText">
-                  {item.amount && item.amount !== 0 && (
-                    <p>
-                      {/* {item.currency_code} {item.amount ? item.amount : "0"}
-                       {item.amount.toString().includes(".") ? "" : ".00"} */}
-                      {item.currency_code} {item.amount.toFixed(2)}
-                      {/* {item.currency_code} {new Intl.NumberFormat().format(item.amount)} */}
-                    </p>
-                  )}
-
-                  <span>
-                    {item.category_type_id == CLASSIFIED_CATEGORY_TYPE.JOBOFFER
-                      ? item.earning_name
-                      : item.currency_name}
-                    {item.is_negotiable ? "(Negotiable)" : ""}
-                  </span>
-                </div>
-              ) : (
-                ""
-              )}
-
-              <div className="classiFieds_countryName ">
-                <span>
-                  <img src={mapicon} alt={mapicon} />
-                  <span>
-                    {(item.country_name)?item.country_name:""}{(item.province_name)?', '+item.province_name:""}{(item.city)?', '+item.city:""}
-                  </span>
-                </span>
-              </div>
-              <div className="classiFieds_aboutText">
-                <p>{item.description}</p>
-              </div>
-              <div className="classiFields_contactPerson">
-                <ContactPerson
-                  forSaleListData={forSaleListData}
-                  index={index}
-                />
-                {item.whatapp_contact_number.length > 0 && (
-                  <WhatsApp watsApp={true} />
-                )}
-              </div>
-
-              <SocialMedaiShare />
-              <button className="edit_DeleteButton" onClick={()=>navigate("/Post-advert",{state:item})}>EDIT / DELETE ADVERT</button>
-                  <button className="not_live">NOT LIVE - Pending Approvals</button>
             </div>
           );
         })}
