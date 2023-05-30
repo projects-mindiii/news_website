118dedc034eaca2e9bbdeab8c96341e35567a663
Nikita soni <nikitasoni.mindiii@gmail.com>
2023-05-30 17:53:04 +0530

    working on linkedin login process

--- src/authComponents/CommonSocial/AppleSocialLogin.js
+++ src/authComponents/CommonSocial/AppleSocialLogin.js
@@ -1,22 +1,22 @@
 import React from "react";
-import AppleLogin from "react-apple-login";
 import Apple from "../../assets/images/apple_logo.png";
-import axios from "axios";
 import { Toast } from "../../utils/Toaster";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { STATUS_CODES } from "../../utils/StatusCode";
 import { SOCIAL_TYPE } from "../../utils/Constants";
+import AppleSignin from "react-apple-signin-auth";
 import { isSocialLogin, socialSignup } from "../../store/slices/UserSlice";
 
 function AppleSocialLogin(props) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
- 
-// function for apple login
+
+  // function for apple login
   const responseApple = async (response) => {
-    if(!response.error) {
-    console.log(response)
+    console.log(response);
+    if (!response.error) {
+      console.log(response);
       let requestData = new FormData();
       requestData.append("social_id", response.authorization.id_token);
       requestData.append("social_type", SOCIAL_TYPE.APPLE);
@@ -54,25 +54,27 @@ function AppleSocialLogin(props) {
           });
         }
       });
-     }
     }
-  
+  };
 
   return (
     <>
       <img src={Apple} alt="apple-logo" />
-      <AppleLogin
-        clientId="qvaring.reactJs.com"
-        redirectURI={process.env.REACT_APP_APPLE_REDIRECT_KEY}
-        usePopup={true}
-        callback={responseApple} 
-        scope="email name"
-        responseMode="query"
-        render={(
-          renderProps
-        ) => (
-          <div onClick={renderProps.onClick}>
-            {props.appleText &&<h3>{props.appleText}</h3>}
+      <AppleSignin
+        authOptions={{
+          clientId: "qvaring.reactJs.com",
+          scope: "email name",
+          redirectURI: process.env.REACT_APP_APPLE_REDIRECT_KEY,
+          state: "",
+          nonce: "nonce",
+          usePopup: true,
+        }}
+        className="apple-auth-btn"
+        onSuccess={() => responseApple}
+        onError={(error) => console.error(error)}
+        render={(renderProps) => (
+          <div onClick={renderProps.onClick} disabled={renderProps.disabled}>
+            {props.appleText && <h3>{props.appleText}</h3>}
           </div>
         )}
       />
